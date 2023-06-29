45230552a2decb0b2092865093883e023fb91c8f
chriss1525 <okothchris15@gmail.com>
2023-06-29 16:57:48 +0300

    change route file to main app file

--- app.py
+++ app.py
@@ -3,7 +3,55 @@
 
 from website import create_app
 
-app = create_app()
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+from flask import Flask, request
+
+db = SQLAlchemy()
+
+DB_NAME = "medicode.db"
+
+# initialize the app
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey?'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+db.init_app(app)
+
+response = ""
+
+@app.route('/ussd', methods=['POST'])
+def ussd_callback():
+    global response
+    session_id = request.values.get("sessionId", None)
+    service_code = request.values.get("serviceCode", None)
+    phone_number = request.values.get("phoneNumber", None)
+    text = request.values.get("text", "default")
+    name = ""
+    id_number = ""
+
+    print(text)
+
+    if text == '':
+        response = "CON welcome to medicode \n"
+        response += "1. Register"
+    elif text == '1':
+        response = "CON Please enter your name\n" 
+        name = request.values.get("text", "")
+        response += 
+    elif text == "1*{}".format(name):
+        response = "CON Please enter your ID\n"    
+        id_number = request.values.get("text", "")
+    elif text == "1*{}*{}".format(name, id_number):
+        response = "CON Registration successful!\n"
+        response += "Name: {}\n".format(name)
+        response += "ID: {}\n".format(id_number)
+
+   
+
+    return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- website/route.py
+++ website/route.py
@@ -1,34 +1,39 @@
 from flask import Flask, request
 import os
+
 app = Flask(__name__)
 
 response = ""
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/ussd', methods=['POST', 'GET'])
 def ussd_callback():
-  global response
-  session_id = request.values.get("sessionId", None)
-  service_code = request.values.get("serviceCode", None)
-  phone_number = request.values.get("phoneNumber", None)
-  text = request.values.get("text", "default")
-  return phone_number
+    global response
+    session_id = request.values.get("sessionId", None)
+    service_code = request.values.get("serviceCode", None)
+    phone_number = request.values.get("phoneNumber", None)
+    text = request.values.get("text", "default")
 
-  if text == '':
-    response  = "CON welcome to medicode \n"
-    response += "1. Register"
-    
-  elif text == '1':
-    response = "CON Please enter your details:\n"
-    response += "1. Enter your name\n"
-    name = input("")  
-    response += name
-    response += "\n2. Enter your ID\n"
-    id_number = input("")  
-    response += id_number
+    print(text)
 
-  else:
-      return
+    if text == '':
+        response = "CON welcome to medicode \n"
+        response += "1. Register"
+    elif text == '1':
+        response = "CON Please enter your name\n"
+    elif text == 'name':
+        name = request.values.get("text", "")
+        response = "CON Please enter your ID\n"
+    elif text == 'id':
+        id_number = request.values.get("text", "")
+        response = "CON Registration successful!\n"
+        response += "Name: {}\n".format(name)
+        response += "ID: {}\n".format(id_number)
 
-  return response
+    else:
+        return
+
+    return response
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=os.environ.get('PORT'))
+
